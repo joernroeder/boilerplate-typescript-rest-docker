c5905c287e86a25e27579e1deb7706a93b2604e8
Jörn Röder <kontakt@joernroeder.de>
2017-05-11 15:50:14 +0200

    added up/down get/setPosition

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,14 @@
+export interface IPosition {
+    x: number;
+    y: number;
+}
+
 export class User {
     public x: number;
     public y: number;
 
-    constructor() {
-        this.x = 0;
-        this.y = 0;
+    constructor(x: number = 0, y: number = 0) {
+        this.setPosition({ x, y });
     }
 
     public right(): void {
@@ -13,6 +17,28 @@ export class User {
 
     public left(): void {
         this.x -= 1;
+    }
+
+    public up(): void {
+        this.y += 1;
+    }
+
+    public down(): void {
+        this.y -= 1;
+    }
+
+    public getPosition() : IPosition {
+        return {
+            x: this.x,
+            y: this.y
+        };
+    }
+
+    public setPosition(position: IPosition): void {
+        const { x, y } = position;
+
+        this.x = x;
+        this.y = y;
     }
 
 }
--- test/unit/user.spec.ts
+++ test/unit/user.spec.ts
@@ -8,6 +8,20 @@ describe('User', () => {
         user = new User();
     });
 
+    it ('should accept x and y as starting point', () => {
+        const usr: User = new User(10, 20);
+
+        expect(usr.x).to.equal(10);
+        expect(usr.y).to.equal(20);
+    });
+
+    it ('should fall back to x:0 and y:0 without a starting point', () => {
+        const usr: User = new User();
+
+        expect(usr.x).to.equal(0);
+        expect(usr.y).to.equal(0);
+    });
+
     it ('should have a x property', () => {
         expect(user).to.have.property('x');
     });
@@ -40,5 +54,58 @@ describe('User', () => {
         user.left();
 
         expect(user.x).to.equal(x - 1);
+    });
+
+    it ('should have a up method', () => {
+       expect(user).to.have.property('up');
+       expect(user.up).to.be.a('function');
+    });
+
+    it ('should increment y on up', () => {
+        const y: number = user.y;
+
+        user.up();
+
+        expect(user.y).to.equal(y + 1);
+    });
+
+    it ('should have a down method', () => {
+        expect(user).to.have.property('down');
+        expect(user.down).to.be.a('function');
+    });
+
+    it ('should decrement y on down', () => {
+        const y: number  = user.y;
+
+        user.down();
+
+        expect(user.y).to.equal(y - 1);
+    });
+
+    it ('should have a getPosition method', () => {
+        expect(user).to.have.property('getPosition');
+        expect(user.getPosition).to.be.a('function');
+    });
+
+    it ('should return x and y as position type', () => {
+        const usr: User = new User(15, 30);
+
+        expect(usr.getPosition()).to.deep.equal({
+            x: 15,
+            y: 30
+        });
+    });
+
+    it ('should have a setPosition method', () => {
+        expect(user).to.have.property('setPosition');
+        expect(user.setPosition).to.be.a('function');
+    });
+
+    it ('should update x and y accordingly', () => {
+        const pos = { x: 10, y: 20 };
+
+        user.setPosition(pos);
+
+        expect(user.getPosition()).to.deep.equal(pos);
     });
 });
